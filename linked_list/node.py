d7f3e99b7b8a0e843e4a939199cabeddc746caba
NaivoRZK <andrydev.tech@gmail.com>
2026-06-15 11:08:34 +0300

    [FEAT:] ajouter le liste chainéé de base

---
+++ linked_list/node.py
@@ -0,0 +1,63 @@
+class Noeud:
+    def __init__(self,donne):
+        self.donne=donne
+        self.suivant=None
+#   ma_neoud=Noeud(10)
+
+class ListeChaine:
+    def __init__(self):
+        self.tete=None
+
+    #Ajouter un élément dans le chaine
+    def ajouterElementFin(self, donne):
+        nouveau_noeud = Noeud(donne)
+        if self.tete is None:
+            self.tete = nouveau_noeud
+            return
+
+        courant = self.tete
+        while courant.suivant is not None:
+            courant=courant.suivant
+        
+        courant.suivant=nouveau_noeud
+        
+
+        """
+        ad 30
+         courant = 12
+         suivant=None ----> 30  20,30 ----> 30,None
+        
+        """
+    
+     #Afficher les éléments du liste chainéés
+    def afficher_elements(self):
+            courant=self.tete
+            while courant is not None:
+                print(courant.donne,end="-->")
+                courant=courant.suivant
+            print("None")
+
+
+#Utilisation de la classe
+ma_liste=ListeChaine()
+ma_liste.ajouterElementFin(10)
+ma_liste.ajouterElementFin(20)
+ma_liste.ajouterElementFin(30)
+
+ma_liste.afficher_elements()
+
+
+"""
+      Supprimer un nœud de la liste (par exemple, enlever le 20).
+
+      Ajter un nœud au tout début de la liste (insérer avant la tete).
+"""
+
+
+ 
+
+
+
+        
+
+        
