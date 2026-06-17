46c52ebfd8a81eda528991cfa0e8388419fc4a50
NaivoRZK <andrydev.tech@gmail.com>
2026-06-17 11:56:01 +0300

    [FEAT:] ajouter le fonction ajout élement au début liste et suppresion elements dans le liste

--- linked_list/node.py
+++ linked_list/node.py
@@ -18,7 +18,6 @@ class ListeChaine:
         courant = self.tete
         while courant.suivant is not None:
             courant=courant.suivant
-        
         courant.suivant=nouveau_noeud
         
 
@@ -37,6 +36,34 @@ class ListeChaine:
                 courant=courant.suivant
             print("None")
 
+    #Ajouter un élément aux début de la liste chainée
+    def ajouterElementDebut(self,donne):
+        nouveau_noeud = Noeud(donne)
+        nouveau_noeud.suivant= self.tete
+        self.tete=nouveau_noeud
+
+    #Supprimer un élément de la liste chainée
+    def supprimerElementListe(self,donne):
+        if self.tete is None:
+            return
+        if self.tete.donne == donne:
+            self.tete=self.tete.suivant
+        
+        courant=self.tete
+        while courant.donne is not None:
+            if  courant.suivant.donne == donne:
+                courant.suivant=courant.suivant.suivant
+                return
+            courant=courant.suivant
+
+        
+
+        
+
+        
+
+
+
 
 #Utilisation de la classe
 ma_liste=ListeChaine()
@@ -44,6 +71,14 @@ ma_liste.ajouterElementFin(10)
 ma_liste.ajouterElementFin(20)
 ma_liste.ajouterElementFin(30)
 
+ma_liste.afficher_elements()
+
+ma_liste.ajouterElementDebut(5)
+ma_liste.ajouterElementDebut(6)
+ma_liste.ajouterElementDebut(2)
+ma_liste.afficher_elements()
+
+ma_liste.supprimerElementListe(20)
 ma_liste.afficher_elements()
 
 
